71bd03210ea2886d94ef6322fc175cf50ed40a72
Md. Ismail <mdismailcse26@gmail.com>
2023-05-25 18:03:16 +0600

    adding loops

---
+++ loop_times_for-loop_untill.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/ruby
+
+
+for i in 1..10
+	puts i
+end
+
+
+10.times {puts "ismail"}
+
+array = [1,2,3,4,5,6,7,8,9,10]
+
+
+array.each do |x|
+	puts x*2
+end
+
+
+i = 0
+
+loop do
+	print "hi "
+	i +=1
+	break if i ==10
+end
+
